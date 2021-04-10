08459b1fcbdaed30071c9c385333840fe99064ec
김상현 <kshy1019@naver.com>
2021-04-10 22:34:27 +0900

    part 14,15

--- SWEngineering-master/frontend/src/screens/ProductScreen.js
+++ SWEngineering-master/frontend/src/screens/ProductScreen.js
@@ -1,6 +1,6 @@
 /* 제품 클릭 시 화면 */
 
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
 import { detailsProduct } from '../actions/productActions';
@@ -12,12 +12,17 @@ import Rating from '../components/Rating';
 export default function ProductScreen(props) {
     const dispatch = useDispatch();
     const productId = props.match.params.id;
+    const [qty, setQty] = useState(1);
     const productDetails = useSelector((state) => state.productDetails);
     const { loading, error, product } = productDetails;
   
     useEffect(() => {
       dispatch(detailsProduct(productId));
     }, [dispatch, productId]);
+
+    const addToCartHandler = () => {
+      props.history.push(`/cart/${productId}?qty=${qty}`);
+    };
 
     return(
         <div>
@@ -75,9 +80,37 @@ export default function ProductScreen(props) {
                       </div>
                     </div>
                   </li>
-                  <li>
-                    <button className="primary block">Add to Cart</button>
-                  </li>
+                  {product.countInStock > 0 && (
+                     <>
+                       <li>
+                         <div className="row">
+                           <div>Qty</div>
+                           <div>
+                             <select
+                               value={qty}
+                               onChange={(e) => setQty(e.target.value)}
+                             >
+                               {[...Array(product.countInStock).keys()].map(
+                                 (x) => (
+                                   <option key={x + 1} value={x + 1}>
+                                     {x + 1}
+                                   </option>
+                                 )
+                               )}
+                             </select>
+                           </div>
+                         </div>
+                       </li>
+                       <li>
+                         <button
+                           onClick={addToCartHandler} // 위에 addToCartHandler 함수 호출
+                           className="primary block"
+                         >
+                           Add to Cart
+                         </button>
+                       </li>
+                     </>
+                   )}
                 </ul>
               </div>
             </div>
--- SWEngineering-master/frontend/src/store.js
+++ SWEngineering-master/frontend/src/store.js
@@ -1,13 +1,21 @@
 import { applyMiddleware, combineReducers, compose, createStore } from 'redux';
 import thunk from 'redux-thunk';
+import { cartReducer } from './reducers/cartReducers';
 import {productDetailsReducer} from './reducers/productReducers';
 import { productsListReducer } from './reducers/productReducers';
 
 
-const initialState = {};
+const initialState = {
+    cart: {
+      cartItems: localStorage.getItem('cartItems')
+        ? JSON.parse(localStorage.getItem('cartItems'))
+        : [],
+    },
+  };
 const reducer = combineReducers({
     productList: productsListReducer,
     productDetails: productDetailsReducer,
+    cart: cartReducer,
 });
 
 const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
