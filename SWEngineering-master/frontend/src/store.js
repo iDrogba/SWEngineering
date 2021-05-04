dbe12af8f6e6f19b45399383f8c5efcddd4ad584
김상현 <kshy1019@naver.com>
2021-05-05 01:54:04 +0900

    fixed

--- SWEngineering-master/frontend/src/store.js
+++ SWEngineering-master/frontend/src/store.js
@@ -1,7 +1,7 @@
 import { applyMiddleware, combineReducers, compose, createStore } from 'redux';
 import thunk from 'redux-thunk';
 import { cartReducer } from './reducers/cartReducers';
-import { orderCreateReducer, orderDetailsReducer, orderPayReducer} from './reducers/orderReducers';
+import { orderCreateReducer, orderDetailsReducer, } from './reducers/orderReducers';
 import {productDetailsReducer} from './reducers/productReducers';
 import { productsListReducer } from './reducers/productReducers';
 import { userRegisterReducer, userSigninReducer } from './reducers/userReducers';
@@ -30,7 +30,7 @@ const reducer = combineReducers({
     userRegister: userRegisterReducer,
     orderCreate: orderCreateReducer,
     orderDetails: orderDetailsReducer,
-    order: orderPayReducer.apply,
+    
 });
 
 const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
